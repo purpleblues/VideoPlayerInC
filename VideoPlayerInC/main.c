9c18b1f307747ae78196a093a82cfee48ee29a50
Sungmin Kim <purpleblues568@gmail.com>
2018-08-28 21:42:41 +0900

    Initial Commit

---
+++ VideoPlayerInC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  VideoPlayerInC
+//
+//  Created by Purple on 28/08/2018.
+//  Copyright © 2018 Purple. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
